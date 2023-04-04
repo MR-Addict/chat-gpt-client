11dda0650cb9932c38f0f4ce79b91c9444b9ed75
MR-Addict <2750417853@qq.com>
2023-04-04 17:58:33 +0800

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const isGithubAction = process.env.GITHUB_ACTION;
+const githubPagesLink = "https://github.com/" + process.env.GITHUB_REPOSITORY;
+const baseUrl = isGithubAction ? githubPagesLink : "./";
+
 export default defineConfig({
+  base: baseUrl,
   plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+  resolve: { alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }] },
 });
